916df1bc6af394d6e29b660354fe5ae8a1fde67c
MrocznyKapelusz <kacper.starosciak@gmail.com>
2026-04-14 22:04:53 +0200

    fix error

--- infrastructure/vps.tf
+++ infrastructure/vps.tf
@@ -2,7 +2,7 @@ resource "hcloud_server" "this" {
   name        = var.cloudflare_zone_name
   image       = "debian-13"
   server_type = "cx23"
-  ssh_keys    = [data.hcloud_ssh_key.this.id]
+  ssh_keys    = [data.hcloud_ssh_key.default.id]
   public_net {
     ipv4_enabled = true
   }
